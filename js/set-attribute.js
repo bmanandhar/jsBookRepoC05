bb3def9794480a2569c1cbbc39949a32408d23b8
Bijaya Manandhar <manandhar.bijaya@gmail.com>
2017-04-10 13:48:11 -0700

    set-attribute.js being added

---
+++ js/set-attribute.js
@@ -0,0 +1,6 @@
+// Page 234
+var firstItem = document.getElementById('one');// Get the first item
+firstItem.className = 'complete';              // Change its class attribute
+
+var fourthItem = document.getElementsByTagName('li').item(3);// Get fourth item
+el2.setAttribute('class', 'cool');             // Add an attribute to it    
