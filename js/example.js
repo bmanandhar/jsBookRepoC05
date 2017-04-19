6fc396cb7454b5c6a8fb1779bdeb47275f6da682
Bijaya Manandhar <manandhar.bijaya@gmail.com>
2017-04-19 10:11:45 -0700

    more exapmles on DOM being added

--- js/example.js
+++ js/example.js
@@ -14,3 +14,18 @@ var newItemFirst = document.createElement('li');   // Create element
 var newTextFirst = document.createTextNode('kale');// Create text node
 newTextLast.appendChild(newTextLast);              // Add text node to element
 list.insertBofore(newItemFirst, list.firstChild);  // Add element to list
+
+var listItems = document.querySelectorAll('li');   // All <li> elements
+
+// Add a class of cool to all list items
+var i;                                             // Counter variable
+for (i = 0; i < listItems.length; i++) {           // Loop through elements
+  listItems[i].className = 'cool';                 // Change class to cool
+}
+
+// Add number of items in the list to the heading
+var heading = document.querySelector('h2');        // h2 element
+var headingText = heading.firstChild.nodeValue;    // h2 text
+var totalItems = listItems.length;                 // No. of <list> elements
+var newHeading = headingText + '<span>' + totalItems + '</span>'; // Content
+heading.textContent = newHeading;                  // Update h2
